9a253c7021b9656f5709c83562ccf710de6081e1
Rob_Franke <nirvana941@gmx.net>
2025-11-24 20:28:31 +0100

    - updated '.gitignore' - added 'settings.py

---
+++ src/utils/settings.py
@@ -0,0 +1,42 @@
+# settings.py
+# imports 
+import os
+from pathlib import Path
+import json
+
+class Session:
+    def __init__(self):
+        self.env = None
+        self.branch = None
+        self.root = None
+        self.data = None
+        self.venv = None
+        self.url_repo = None
+
+    def save_session(self):
+        if self.root is None:
+            raise RuntimeError("Session.root must be set before saving session.")
+        file_path = Path(self.root) / ".env.session"
+
+        state= {
+        "SESSION_ENV": self.env or "",
+            "SESSION_BRANCH": self.branch or "",
+            "SESSION_ROOT": str(self.root or ""),
+            "SESSION_DATA": str(self.data or ""),
+            "SESSION_VENV": str(self.venv or ""),
+            "SESSION_URL_REPO": self.url_repo or "",
+        }
+
+        with open(file_path, "w") as f:
+            for key, value in state.items():
+                f.write(f"{key}={value}\n")
+    
+    # export_to_os(self):
+    #     
+    # # def __init__(self):
+    # #     self.inputs = {}
+
+    # def record(self, key, value):
+    #     self.inputs[key] = value
+
+session = Session()
