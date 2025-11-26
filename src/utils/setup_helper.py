c32dbc24baad320f82d4da271f494bf67d585a93
Rob_Franke <nirvana941@gmx.net>
2025-11-26 14:37:27 +0100

    feat: Refactor environment variable loading to improve efficiency and clarity

--- src/utils/setup_helper.py
+++ src/utils/setup_helper.py
@@ -57,26 +57,21 @@ def load_env_vars():
     Load environment variables from an .env file if available.
     """
 
+    if session.env_loaded:
+        return
+    
     env_path = find_dotenv()
-    
-    try:
-        session_path = find_dotenv(filename=".env.session")
-
-        if session_path:
-            load_dotenv(session_path)
-            print("Variable from .env.session loaded")
-
-        if session.env_loaded:
-            return None
-        
-        if env_path:
-            load_dotenv(env_path)
-            print("Variable from .env loaded")
-            session.env_loaded = True
-    
-    except Exception:
+    if env_path:
         load_dotenv(env_path)
-        print("Variable from .env loaded")
+        print("Variables from .env loaded")
+
+    session_path = find_dotenv(filename=".env.session")
+    if session_path and os.path.exists(session_path):
+        load_dotenv(session_path, override=True)
+        print("Variables from .env.session loaded")
+    
+    session.env_loaded = True
+ 
         
 
 def info_as_string(df):
