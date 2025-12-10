0ffb5bb0a5b05f0d8dd4d7ef14e68bb84f89f120
Rob_Franke <nirvana941@gmx.net>
2025-12-10 13:40:11 +0100

    Refactor fetch_files to use src and dst paths
    Updated fetch_files function to accept source and destination paths, and modified file movement logic accordingly.

--- src/A_fetch_files.py
+++ src/A_fetch_files.py
@@ -5,39 +5,41 @@ import utils.setup_helper as sh
 from utils.settings import session
 import utils.file_helper as fh 
 
-def fetch_files(path=None, file_type=None):
-    if not path:
-        # load env variables from .env and .env.session
-        sh.load_env_vars()
+def fetch_files(src_path=None, dst_path=None, file_type=None):
+    if not src_path or not dst_path:
+        # # load env variables from .env and .env.session
+        # sh.load_env_vars()
 
-        # load paths from .env
-        sh.get_paths()
-        # print("[DEBUG] ROOT loaded =", session.root)
-        # ROOT = Path(session.root)
-        DATA = Path(session.data)
+        # # load paths from .env
+        # sh.get_paths()
+        # # print("[DEBUG] ROOT loaded =", session.root)
+        # # ROOT = Path(session.root)
+        # DATA = Path(session.data)
 
-        INPUT = DATA / "data_input"
-        INPUT.mkdir(parents=True, exist_ok=True) 
+        # INPUT = DATA / "data_input"
+        # INPUT.mkdir(parents=True, exist_ok=True) 
 
-        path = INPUT
+        # path = INPUT
+        print("At least one file path is  not given.")
 
     if not file_type:
         file_type = [".csv", 
                      # ".json"
                      ]
 
-    folder = Path(path)
-    files = [str(f) for f in folder.iterdir() if f.suffix.lower() in file_type]
+    folder = Path(src_path)
+    files = [str(f) for f in folder.iterdir() \
+             if f.suffix.lower() in file_type]
     
     if not files:
         print("No new files found")
-        return None
+        return []
     
     files_new = []
     for f in files:
         # check for columns
         # to be added
-        f_moved = fh.move_file(f, path)
+        f_moved = fh.move_file(f, dst_path)
         files_new.append(str(f_moved))
 
     print(f"Found {len(files)} files in 'data_input' folder and moved them to folder 'data_lake'")
